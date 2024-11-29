504512faefd19c72016be242abfc8eea92f7444b
Promise Umeh <umehpromise1000@gmail.com>
2024-11-29 23:25:20 +0100

    feat: test for transfers added

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,92 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/umeh-promise/simple_bank/util"
+)
+
+func createRandomTransfer(t *testing.T) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: 10,
+		ToAccountID:   11,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.NotZero(t, transfer.Amount)
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t)
+
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestUpdateTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+
+	arg := UpdateTransferParams{
+		ID:     transfer1.ID,
+		Amount: util.RandomMoney(),
+	}
+
+	err := testQueries.UpdateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, arg.Amount, transfer2.Amount)
+}
+
+func TestDeleteTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+
+	err := testQueries.DeleteTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, transfer2.ID)
+}
+
+func TestListTransfers(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t)
+	}
+
+	arg := ListTransfersParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
