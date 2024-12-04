8b92244c315b74891c15432e7925f85feeed9a67
Promise Umeh <umehpromise1000@gmail.com>
2024-12-04 17:38:37 +0100

    feat: added an entry point for server/db connection

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/umeh-promise/simple_bank/api"
+	db "github.com/umeh-promise/simple_bank/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:professor@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+
+	conn, err := sql.Open(dbDriver, dbSource)
+
+	if err != nil {
+		log.Fatal("Cannot connect to the database", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+
+	if err != nil {
+		log.Fatal("Cannot start server")
+	}
+}
